33d7a3b756e8f7ea33897c18255281e1fee04778
AryaFrandika <aryafrandikadaulay@student.telkomuniversity.ac.id>
2023-03-19 22:11:27 +0700

    menambahkan class HaloGeneric dengan method  SapaUser

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿
+﻿using System;
 
+public class HaloGeneric<T>
+{
+    private T _data;
 
+    public HaloGeneric(T data)
+    {
+        _data = data;
+    }
+
+    public void SapaUser<U>(U user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nama = "Arya";
+        HaloGeneric<string> sapa = new HaloGeneric<string>(" ");
+        sapa.SapaUser(nama);  
+    }
+}
