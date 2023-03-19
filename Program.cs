818a333f0c3cc668afdf8971e2a47603a73f4606
AryaFrandika <aryafrandikadaulay@student.telkomuniversity.ac.id>
2023-03-19 22:24:28 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,3 +1,46 @@
-﻿
+﻿using System;
 
+public class HaloGeneric<T>
+{
+    private T _data;
 
+    public HaloGeneric(T data)
+    {
+        _data = data;
+    }
+
+    public void SapaUser<U>(U user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nama = "Arya";
+        HaloGeneric<string> sapa = new HaloGeneric<string>(nama);
+        sapa.SapaUser(nama);
+
+        string nim = "1302213069";
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
+    }
+
+}
